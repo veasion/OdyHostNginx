22518d851c8253aa666a88bb6b03c9b9eb8401cc
veasion <veasion@qq.com>
2019-09-23 00:38:36 +0800

    modify code

--- Test.cs
+++ Test.cs
@@ -11,8 +11,7 @@ namespace OdyHostNginx
     {
         public static void test()
         {
-            List<ProjectConfig> projects = OdyConfigHelper.loadConfig(@"C:\Users\Veasion\Desktop\test");
-            OdyConfigHelper.writeConfig(projects, @"C:\Users\Veasion\Desktop\test2");
+
         }
 
     }
